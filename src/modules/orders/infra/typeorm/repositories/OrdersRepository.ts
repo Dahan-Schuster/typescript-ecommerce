8026e70edbd6986ad836981ab899d555eb62dbfa
Dahan Schuster <dan.plschuster@gmail.com>
2020-08-25 13:32:26 -0300

    Módulo orders
    Implementa o repositório de pedidos

--- src/modules/orders/infra/typeorm/repositories/OrdersRepository.ts
+++ src/modules/orders/infra/typeorm/repositories/OrdersRepository.ts
@@ -5,19 +5,29 @@ import ICreateOrderDTO from '@modules/orders/dtos/ICreateOrderDTO';
 import Order from '../entities/Order';
 
 class OrdersRepository implements IOrdersRepository {
-  private ormRepository: Repository<Order>;
+	private ormRepository: Repository<Order>;
 
-  constructor() {
-    this.ormRepository = getRepository(Order);
-  }
+	constructor() {
+		this.ormRepository = getRepository(Order);
+	}
 
-  public async create({ customer, products }: ICreateOrderDTO): Promise<Order> {
-    // TODO
-  }
+	public async create({
+		customer,
+		products,
+	}: ICreateOrderDTO): Promise<Order> {
+		const order = this.ormRepository.create({
+			customer,
+			order_products: products,
+		});
 
-  public async findById(id: string): Promise<Order | undefined> {
-    // TODO
-  }
+		await this.ormRepository.save(order);
+
+		return order;
+	}
+
+	public async findById(id: string): Promise<Order | undefined> {
+		return this.ormRepository.findOne(id);
+	}
 }
 
 export default OrdersRepository;
