1d5807222da4649fca523325c7a45bb2b55b2cbb
Dahan Schuster <dan.plschuster@gmail.com>
2020-08-25 00:07:28 -0300

    Módulo products
    Implementa o controlador de products

--- src/modules/products/infra/http/controller/ProductsController.ts
+++ src/modules/products/infra/http/controller/ProductsController.ts
@@ -4,7 +4,19 @@ import { container } from 'tsyringe';
 import CreateProductService from '@modules/products/services/CreateProductService';
 
 export default class ProductsController {
-  public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
-  }
+	public async create(
+		request: Request,
+		response: Response,
+	): Promise<Response> {
+		const { name, price, quantity } = request.body;
+		const createProductService = container.resolve(CreateProductService);
+
+		const product = await createProductService.execute({
+			name,
+			price,
+			quantity,
+		});
+
+		return response.json(product);
+	}
 }
