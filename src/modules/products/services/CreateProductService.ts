9e1141989e9441295dee2fb7bbc03823bc7369f8
Dahan Schuster <dan.plschuster@gmail.com>
2020-08-25 00:07:20 -0300

    Módulo products
    Implementa o serviço de criação de products

--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -6,18 +6,38 @@ import Product from '../infra/typeorm/entities/Product';
 import IProductsRepository from '../repositories/IProductsRepository';
 
 interface IRequest {
-  name: string;
-  price: number;
-  quantity: number;
+	name: string;
+	price: number;
+	quantity: number;
 }
 
 @injectable()
 class CreateProductService {
-  constructor(private productsRepository: IProductsRepository) {}
+	constructor(
+		@inject('ProductsRepository')
+		private productsRepository: IProductsRepository,
+	) {}
 
-  public async execute({ name, price, quantity }: IRequest): Promise<Product> {
-    // TODO
-  }
+	public async execute({
+		name,
+		price,
+		quantity,
+	}: IRequest): Promise<Product> {
+		const productAlreadyExists = await this.productsRepository.findByName(
+			name,
+		);
+
+		if (productAlreadyExists)
+			throw new AppError(
+				'A product with the same name was already created',
+			);
+
+		return this.productsRepository.create({
+			name,
+			price,
+			quantity,
+		});
+	}
 }
 
 export default CreateProductService;
